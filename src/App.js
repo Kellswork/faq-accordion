51c4b20a8e9e3b1a85e2c6ff9e07484964b12d25
Kellswork <kellogbonna@gmail.com>
2024-09-15 15:12:03 +0100

    add logic to hide and show text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
+import { useState } from "react";
 import backgroundImage from "./assets/images/background-pattern-desktop.svg";
 import iconPlus from "./assets/images/icon-plus.svg";
 import iconMinus from "./assets/images/icon-minus.svg";
 import iconStar from "./assets/images/icon-star.svg";
+import Data from "./Data";
 
 function App() {
+  const [showIndex, setShowIndex] = useState(null);
+  const [show, setShow] = useState(false)
+  ;
+  const handleShowAnsweer = (index) => {
+    if((showIndex === index) && (show === true)){
+      setShow(false)
+    } else setShow(!show)
+    setShowIndex(index);
+  };
+
   return (
     <div className="main-container">
       <div className="container-background-image">
@@ -13,48 +25,38 @@ function App() {
       <div className="card-layout">
         <div className="card-container">
           <div className="header">
-            <img src={iconStar} alt="minus star" />
+            <img src={iconStar} alt="icon star" />
             <h1>FAQs</h1>
           </div>
           <div className="card-content">
-            <div className="card-content-q-and-a">
-              <div className="question">
-                <h3>What is Frontend Mentor, and how will it help me?</h3>
-                <img src={iconMinus} alt="minus icon" />
-              </div>
-              <div className="answer">
-                Frontend Mentor offers realistic coding challenges to help
-                developers improve their frontend coding skills with projects in
-                HTML, CSS, and JavaScript. It’s suitable for all levels and
-                ideal for portfolio building.
-              </div>
-            </div>
-            <div className="divider"></div>
-            <div className="card-content-q-and-a">
-              <div className="question">
-                <h3>Is Frontend Mentor free?</h3>
-                <img src={iconPlus} alt="plus icon" />
-              </div>
-              <div className="answer">
-                Frontend Mentor offers realistic coding challenges to help
-                developers improve their frontend coding skills with projects in
-                HTML, CSS, and JavaScript. It’s suitable for all levels and
-                ideal for portfolio building.
-              </div>
-            </div>
-            <div className="divider"></div>
-            <div className="card-content-q-and-a">
-              <div className="question">
-                <h3>Can I use Frontend Mentor projects in my portfolio?</h3>
-                <img src={iconPlus} alt="plus icon" />
-              </div>
-              <div className="answer">
-                Frontend Mentor offers realistic coding challenges to help
-                developers improve their frontend coding skills with projects in
-                HTML, CSS, and JavaScript. It’s suitable for all levels and
-                ideal for portfolio building.
-              </div>
-            </div>
+            {Data.map((data, index) => (
+              <>
+                <div key={data.question} className="card-content-q-and-a">
+                  <div className="question">
+                    <button className="quest-btn" onClick={()=>{handleShowAnsweer(index)}}><h3>{data.question}</h3></button>
+                    <button className="icon-btn"
+                      onClick={() => {
+                        handleShowAnsweer(index);
+                      }}
+                    >
+                      {(showIndex === index && show===true) ? (
+                        <img className="minus-icon" src={iconMinus} alt="minus icon"  />
+                      ) : (
+                        <img src={iconPlus} alt="plus icon" />
+                      )}
+                    </button>
+                  </div>
+                  <div
+                    className={
+                     ( showIndex === index && show===true )? "showAnswer answer" : "answer"
+                    }
+                  >
+                    {data.answer}
+                  </div>
+                </div>
+                <div className="divider"></div>
+              </>
+            ))}
           </div>
         </div>
       </div>
---
+++ src/Data.js
@@ -0,0 +1,32 @@
+const Data = [
+  {
+    question: "What is Frontend Mentor, and how will it help me?",
+    answer: `Frontend Mentor offers realistic coding challenges to help
+    developers improve their frontend coding skills with projects in
+    HTML, CSS, and JavaScript. It’s suitable for all levels and
+    ideal for portfolio building.`,
+  },
+  {
+    question: "Is Frontend Mentor free?",
+    answer: ` Frontend Mentor offers realistic coding challenges to help
+                developers improve their frontend coding skills with projects in
+                HTML, CSS, and JavaScript. It’s suitable for all levels and
+                ideal for portfolio building.`,
+  },
+  {
+    question: "Can I use Frontend Mentor projects in my portfolio?",
+    answer: `Frontend Mentor offers realistic coding challenges to help
+                developers improve their frontend coding skills with projects in
+                HTML, CSS, and JavaScript. It’s suitable for all levels and
+                ideal for portfolio building.`,
+  },
+  {
+    question: "How can I get help if I'm stuck on a challenge?",
+    answer: ` Frontend Mentor offers realistic coding challenges to help
+    developers improve their frontend coding skills with projects in
+    HTML, CSS, and JavaScript. It’s suitable for all levels and
+    ideal for portfolio building.`,
+  },
+];
+
+export default Data;
